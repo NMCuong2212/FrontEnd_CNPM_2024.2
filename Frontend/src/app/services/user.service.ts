33b5c59be76b92c9b7ca347df94f7d6cb6f0793c
Nguyen Manh Cuong <Cuong.NM225268@sis.hust.edu.vn>
2025-04-10 22:06:57 +0700

    Thêm forgot password UI và chỉnh sửa user service

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -15,4 +15,12 @@ export class UserService {
       }
     )
   }
+
+  forgotPassword(data:any){
+    return this.httpClient.post(this.url+
+      "/user/forgotPassword",data,{
+        headers:new HttpHeaders().set('Content-Type','application/json')
+      }
+    )
+  }
 }
