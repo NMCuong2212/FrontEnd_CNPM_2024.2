b4e6dee373ee3316ab30689fc878e7ec783ba466
donThe04 <dongvanthe2606@gmail.com>
2025-04-15 13:39:30 +0700

    Push code từ bản tải .zip lên nhánh mới

---
+++ Frontend/src/app/shared/global-constants.ts
@@ -0,0 +1,20 @@
+export class GlobalConstants{
+    
+    //Message
+    public static genericError: string = "Something went wrong.Please try again later";
+
+    //regex
+    public static nameRegex:string = "[a-zA-Z0-9 ]*";
+
+    public static emailRegex:string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+
+    public static contactNumberRegex: string = "^[0-9]{10,11}$";
+
+
+    //variable
+    public static error:string = "error";
+    
+
+
+
+}
---
+++ Frontend/src/app/signup/signup.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { SnackbarService } from '../services/snackbar.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { GlobalConstants } from '../shared/global-constants';
+import { error } from 'console';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  password = false;
+  confirmPassword = false;
+  signupForm!: FormGroup;
+  responseMessage:any;
+
+  constructor(private formBuilder:FormBuilder,
+    private router:Router,
+    private userService:UserService,
+    private snackbarService:SnackbarService,
+    public dialogRef:MatDialogRef<SignupComponent>,
+    private ngxService:NgxUiLoaderService
+
+  ) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      email: [null, [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber: [null, [Validators.required, Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]]
+    });
+
+    console.log("Signup Form:", this.signupForm);
+}
+
+  validateSubmit() {
+    return this.signupForm.controls['password'].value !== this.signupForm.controls['confirmPassword'].value;
+  }
+  
+  handleSubmit() {
+    if (!this.signupForm.valid || this.validateSubmit()) {
+      this.snackbarService.openSnackBar("Vui lòng kiểm tra lại thông tin!", GlobalConstants.error);
+      return;
+    }
+  
+    this.ngxService.start();
+    
+    const formData = this.signupForm.value;
+    const data = {
+      name: formData.name,
+      email: formData.email,
+      contactNumber: formData.contactNumber,
+      password: formData.password
+    };
+  
+    this.userService.signup(data).subscribe(
+      (response: any) => {
+        this.ngxService.stop();
+        this.dialogRef.close();
+        this.responseMessage = response?.message || "Đăng ký thành công!";
+        this.snackbarService.openSnackBar(this.responseMessage, "");
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        this.ngxService.stop();
+        this.responseMessage = error.error?.message || GlobalConstants.genericError;
+        this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+      }
+    );
+  }
+  
+}
+
+
